adcbd336ed79175d2d5b1ddeb117d57b2243f41b
Cihan Cakir <cihancakir@Cihans-MacBook-Air.local>
2020-01-05 23:12:47 +0300

    Task 3 Finish

--- App/todoapp/lib/main.dart
+++ App/todoapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/UI/Intray/intray_page.dart';
 import 'models/global.dart';
 
 void main() => runApp(MyApp());
@@ -41,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: [
                   new Container(
                     color: darkGreyColor,
+                    child: IntrayPage(),
                   ),
                   new Container(
                     color: Colors.orange,
