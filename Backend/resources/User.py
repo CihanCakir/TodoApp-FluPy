be921606d23a3ad91b6628f85aa3617a95663646
Cihan Cakir <cihancakir@Cihans-MacBook-Air.local>
2020-01-06 10:01:09 +0300

    Good

--- Backend/resources/User.py
+++ Backend/resources/User.py
@@ -2,5 +2,12 @@ from flask_restful import Resource
 
 
 class User(Resource):
+    def __init__(self, user_id, api_key, fistname, lastname, email, password):
+        self.user_id = user_id
+        self.api_key = api_key
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+
     def get(self):
         return {"message": "Hello, Kalle!"}
